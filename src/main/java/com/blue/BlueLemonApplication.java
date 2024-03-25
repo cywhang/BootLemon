8e743788ad9be5898c83bd6dc07575bc24e5ba37
chan <cywhang2@naver.com>
2024-03-26 04:46:32 +0900

    parameter store 적용중 로그 찍어보기

--- src/main/java/com/blue/BlueLemonApplication.java
+++ src/main/java/com/blue/BlueLemonApplication.java
@@ -1,14 +1,28 @@
 package com.blue;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class BlueLemonApplication {
+
+	@Value("${cloud.aws.credentials.access-key}")
+	private String accessKey;
+
 	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(BlueLemonApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner parameterLogger() {
+		return args -> {
+			System.out.println("My accessKey value: " + accessKey);
+		};
 	}
 }
